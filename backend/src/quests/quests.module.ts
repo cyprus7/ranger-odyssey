e52ce31ca3db65193e40e48cde7ae4547ce44ef8
Aleksei Chernov <cyprus2047@gmail.com>
2025-08-15 12:57:32 +0300

    feat: add ObservabilityLoggingModule to QuestsModule imports

--- backend/src/quests/quests.module.ts
+++ backend/src/quests/quests.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuestsController, RewardsController } from './quests.controller';
 import { QuestsService } from './quests.service';
+import { ObservabilityLoggingModule } from '../observability/logging.module';
 
 @Module({
+  imports: [ObservabilityLoggingModule],
   controllers: [QuestsController, RewardsController],
   providers: [QuestsService],
 })
