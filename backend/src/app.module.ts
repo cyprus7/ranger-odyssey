cf19c46db3b2ae2fb4583fa1e51913e6805cafe8
Aleksei Chernov <cyprus2047@gmail.com>
2025-08-16 15:46:52 +0300

    refactor: remove unused modules and update dependencies in package.json and package-lock.json

--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { QuestsModule } from './quests/quests.module'
-import { HealthController } from './health.controller'
-
-@Module({
-    imports: [QuestsModule],
-    controllers: [HealthController],
-})
-export class AppModule {}
